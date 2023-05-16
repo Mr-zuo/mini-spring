ccacfe1d58bb8d4843351e2349a7e68d7a4bb6fa
10020093 <zhangxiaolong3@shein.com>
2023-05-16 17:28:55 +0800

    fix bug

--- src/test/java/org/springframework/test/ioc/common/CustomBeanFactoryPostProcessor.java
+++ src/test/java/org/springframework/test/ioc/common/CustomBeanFactoryPostProcessor.java
@@ -15,9 +15,10 @@ public class CustomBeanFactoryPostProcessor implements BeanFactoryPostProcessor 
 
 	@Override
 	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-		BeanDefinition personBeanDefiniton = beanFactory.getBeanDefinition("person");
-		PropertyValues propertyValues = personBeanDefiniton.getPropertyValues();
+		BeanDefinition personBeanDefinition = beanFactory.getBeanDefinition("person");
+		PropertyValues propertyValues = personBeanDefinition.getPropertyValues();
 		//将person的name属性改为ivy
 		propertyValues.addPropertyValue(new PropertyValue("name", "ivy"));
+		personBeanDefinition.setPropertyValues(propertyValues);
 	}
 }
